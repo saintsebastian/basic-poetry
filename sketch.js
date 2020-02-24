48d65ec448713c9249bb30ce6920431af1186cb9
saintsebastian <tilitidam@gmail.com>
2020-02-24 18:59:39 +0100

    rhyme every word

--- sketch.js
+++ sketch.js
@@ -19,14 +19,33 @@ function processRita() {
   let s = input.value();
   let rs = new RiString(s);
   let words = rs.words();
-  console.log(words);
+  let syllables = RiTa.getSyllables(s);
+  console.log(syllables);
 
   let newLine = "";
-  for (word of words) {
-    newLine += `${word} `;
-  }
-
+  words.forEach((word, i) => {
+    if (isPunctuation(word)) {
+      let rWord = new RiString(word);
+      console.log(rWord.analyze());
+      let rhyme = findRhyme(word)
+      newLine += `${rhyme ? rhyme : 'oh'} `;
+    }
+  })
   createP(newLine);
+}
+
+function findRhyme(word) {
+  let syllables = RiTa.getSyllables(word).split('/')
+  console.log(syllables);
+  let rhyme = RiTa.rhymes(word).find((rhyme) => {
+    return syllables.length === RiTa.getSyllables(rhyme).split('/').length
+  });
+  return rhyme
+}
+
+function isPunctuation(word) {
+  let punc = ['.', ',', ';', ':']
+  return punc.indexOf(word) === -1
 }
 
 // todo rhyme last word in the line
